d1b876d5e7c76b47ee3452f518355a9993c4b90e
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-08 10:31:45 +0800

    update remove items

--- cart_update.php
+++ cart_update.php
@@ -27,14 +27,32 @@ while ($row = mysqli_fetch_assoc($result)) {
         {
             unset($_SESSION["cart_products"][$new_product['item_id']]); //unset old array item
         }
-       
     }
     $_SESSION["cart_products"][$new_product['item_id']] = $new_product;
-    
+}
+if (isset($_POST["product_qty"]) || isset($_POST["remove_code"])) {
+    // var_dump($_POST["remove_code"]);
+    //update item quantity in product session
+
+    if (isset($_POST["product_qty"]) && is_array($_POST["product_qty"])) {
+
+        foreach ($_POST["product_qty"] as $key => $value) {
+            if (is_numeric($value)) {
+                // var_dump( $key, $value);
+                $_SESSION["cart_products"][$key]["item_qty"] = $value;
+            }
+        }
+    }
+
+    if (is_array($_POST["remove_code"])) {
+        foreach ($_POST["remove_code"] as $key) {
+            var_dump($key);
+            unset($_SESSION["cart_products"][$key]);
+        }
+    }
 }
 // print_r($new_product);
-echo "<pre>";
-print_r($_SESSION['cart_products']);
-//print_r($new_product);
-echo "</pre>";
-?>
+// echo "<pre>";
+// print_r($_SESSION['cart_products']);
+// //print_r($new_product);
+// echo "</pre>";
